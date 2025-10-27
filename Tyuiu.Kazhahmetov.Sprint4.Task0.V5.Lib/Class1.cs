20c1f1381a192c101318b7f98fda24521f92231a
Таджикович Ержан <kazamircik@gmail.com>
2025-10-27 21:48:42 +0500

    Add project files.

---
+++ Tyuiu.Kazhahmetov.Sprint4.Task0.V5.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.Kazhahmetov.Sprint4.Task0.V5.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.Kazhahmetov.Sprint4.Task0.V5.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.Kazhahmetov.Sprint4.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
