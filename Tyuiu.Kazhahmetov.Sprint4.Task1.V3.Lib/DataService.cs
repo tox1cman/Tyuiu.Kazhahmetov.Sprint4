10ac068f6c15b98ee3c952e6e9b371b9a2d7ba3a
Таджикович Ержан <kazamircik@gmail.com>
2025-10-28 20:40:42 +0500

    Task 1

---
+++ Tyuiu.Kazhahmetov.Sprint4.Task1.V3.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.Kazhahmetov.Sprint4.Task1.V3.Lib
+{
+    public class DataService : ISprint4Task1V3
+    {
+        public int Calculate(int[] array)
+        {
+            int p = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    p *= array[i];
+                }
+            }
+            return p;
+        }
+    }
+}
---
+++ Tyuiu.Kazhahmetov.Sprint4.Task1.V3/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.Kazhahmetov.Sprint4.Task1.V3.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #4 | Выполнил: Кажахметов А. Е. | ПИНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #5                                                              *");
+Console.WriteLine("* Выполнил: Кажахметов Амир Ерболович | ПИНб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int len;
+Console.WriteLine("Сколько элементов:");
+len = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[len];
+
+for (int i = 0; i < len; i++)
+{
+    Console.WriteLine("Введите значение " + i + " элемента массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("Массив: ");
+for (int i = 0;i < len; i++)
+{
+    Console.WriteLine(array[i] + "\t");
+}
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.Calculate(array);
+Console.WriteLine(res);
+Console.ReadKey();
