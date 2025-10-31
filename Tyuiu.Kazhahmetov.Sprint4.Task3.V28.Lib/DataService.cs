b17654066d8ef67b3dbb8fed74f6d914080f5cc0
Таджикович Ержан <kazamircik@gmail.com>
2025-11-01 01:41:18 +0500

    Task 3

---
+++ Tyuiu.Kazhahmetov.Sprint4.Task3.V28.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.Numerics;
+using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.Kazhahmetov.Sprint4.Task3.V28.Lib
+{
+    public class DataService : ISprint4Task3V28
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int cols = matrix.Length / rows;
+
+            int res = 10;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (j == 3)
+                    {
+                        if (matrix[i, j] < res)
+                        {
+                            res = matrix[i, j];
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.Kazhahmetov.Sprint4.Task3.V28/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.Kazhahmetov.Sprint4.Task3.V28.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #4 | Выполнил: Кажахметов А. Е. | ПИНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #28                                                             *");
+Console.WriteLine("* Выполнил: Кажахметов Амир Ерболович | ПИНб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int[,] mas = new int[5, 5] { { 5, 4, 9, 8, 5 }, { 4, 6, 6, 9, 9 }, { 9, 8, 4, 8, 8 }, { 6, 8, 6, 8, 9 }, { 9, 4, 6, 6, 7 } };
+
+int rows = mas.GetUpperBound(0) + 1;
+int cols = mas.Length / rows;
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write($"{mas[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.Calculate(mas);
+Console.WriteLine(res);
+Console.ReadKey();  
