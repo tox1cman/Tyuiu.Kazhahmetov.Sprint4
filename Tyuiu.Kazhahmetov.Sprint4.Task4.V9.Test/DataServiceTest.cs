f0698c9a18ff10e01f137596cf79c761367b1381
Таджикович Ержан <kazamircik@gmail.com>
2025-11-01 20:35:51 +0500

    Task 4

---
+++ Tyuiu.Kazhahmetov.Sprint4.Task4.V9.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.Kazhahmetov.Sprint4.Task4.V9.Lib;
+namespace Tyuiu.Kazhahmetov.Sprint4.Task4.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = { { 3, 7, 3, 1, 5 }, { 6, 3, 2, 1, 2 }, { 1, 3, 2, 8, 1 }, { 5, 8, 1, 5, 1 }, { 3, 3, 4, 4, 6 } };
+
+            int res = ds.Calculate(matrix);
+            int wait = 42;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
--- Tyuiu.Kazhahmetov.Sprint4.Task4.V9/Program.cs
+++ Tyuiu.Kazhahmetov.Sprint4.Task4.V9/Program.cs
@@ -1,4 +1,4 @@
-﻿using Tyuiu.Kazhahmetov.Sprint4.Task3.V28.Lib;
+﻿using Tyuiu.Kazhahmetov.Sprint4.Task4.V9.Lib;
 
 DataService ds = new DataService();
 
@@ -7,39 +7,45 @@ Console.Title = "Спринт #4 | Выполнил: Кажахметов А. Е
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* Спринт #4                                                               *");
 Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
-Console.WriteLine("* Задание #3                                                              *");
-Console.WriteLine("* Вариант #28                                                             *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #9                                                              *");
 Console.WriteLine("* Выполнил: Кажахметов Амир Ерболович | ПИНб-25-1                         *");
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
 Console.WriteLine("***************************************************************************");
 
-int[,] mas = new int[5, 5] 
-{ 
-    { 5, 4, 9, 8, 5 },
-    { 4, 6, 6, 9, 9 },
-    { 9, 8, 4, 8, 8 },
-    { 6, 8, 6, 8, 9 },
-    { 9, 4, 6, 6, 7 } 
-};
+Console.WriteLine("Строки: ");
+int rows = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Таблицы: ");
+int cols = Convert.ToInt32(Console.ReadLine());
 
-int rows = mas.GetUpperBound(0) + 1;
-int cols = mas.Length / rows;
+int[,] matrix = new int[rows, cols];
 
 for (int i = 0; i < rows; i++)
 {
     for (int j = 0; j < cols; j++)
     {
-        Console.Write($"{mas[i, j]} \t");
+        Console.WriteLine($"Введите {i}, {j} элемента матрицы: ");
+        matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("\nМассив: ");
+for (int i = 0;i < rows; i++)
+{
+    for ( int j = 0; j < cols; j++)
+    {
+        Console.WriteLine($"{matrix[i, j]} \t");
     }
     Console.WriteLine();
 }
-
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
 Console.WriteLine("***************************************************************************");
 
-int res = ds.Calculate(mas);
+int res = ds.Calculate(matrix);
 Console.WriteLine(res);
-Console.ReadKey();  
+Console.ReadKey();
